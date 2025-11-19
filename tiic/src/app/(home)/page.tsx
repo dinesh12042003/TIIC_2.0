1cc5c47f866e399959a42bf8130cc1a7847d6a5f
Dinesh Paridala <dineshchowdharyparidala@gmail.com>
2025-11-20 02:57:09 +0530

    hero

--- tiic/src/app/(home)/page.tsx
+++ tiic/src/app/(home)/page.tsx
@@ -1,7 +1,20 @@
+import { BackgroundRippleEffect } from "@/components/ui/background-ripple-effect";
+import { Navbar } from "@/components/ui/resizable-navbar";
 import Image from "next/image";
+import { NavbarDemo } from "./components/NavbarDemo";
+import { BackgroundBeamsDemo } from "./components/BackgroundBeamsDemo";
 
 export default function Home() {
   return (
-    <div>Hello</div>
+    
+    
+    <div>
+      <nav className="fixed top-0 z-50 w-full bg-white shadow-md">
+        <NavbarDemo />
+      </nav>
+      <BackgroundBeamsDemo />
+      {/* <BackgroundRippleEffect /> */}
+      <BackgroundBeamsDemo />
+    </div>
   );
 }
